e8c2e85915a593edb91bda7e7d7189ed47879995
LoicFmi <90246399+LoicFmi@users.noreply.github.com>
2023-01-21 11:16:51 +0100

    Correction bug report Login + 2 bug hunt

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -30,21 +30,26 @@ export default class {
   getBills = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+          // Effectue le tri par ordre décroissant
+          bills.sort((a, b) => {
+            return new Date(b.date) - new Date(a.date);
+          });
+          
+          bills.map(doc => {
             try {
               return {
                 ...doc,
                 date: formatDate(doc.date),
                 status: formatStatus(doc.status)
               }
-            } catch(e) {
+            } catch (e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
+              console.log(e, 'for', doc)
               return {
                 ...doc,
                 date: doc.date,
@@ -53,8 +58,8 @@ export default class {
             }
           })
           console.log('length', bills.length)
-        return bills
-      })
+          return bills
+        })
     }
   }
 }
